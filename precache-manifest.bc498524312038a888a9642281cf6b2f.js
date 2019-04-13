c230845426693e594e0d9992304323d341778307
Steve <sliriano2020@gmail.com>
2019-04-13 10:41:42 -0400

    Updates

---
+++ precache-manifest.bc498524312038a888a9642281cf6b2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57c64a37c52c1cb54481",
+    "url": "/EthPolls/static/css/main.8b7f5749.chunk.css"
+  },
+  {
+    "revision": "57c64a37c52c1cb54481",
+    "url": "/EthPolls/static/js/main.7c6a49a5.chunk.js"
+  },
+  {
+    "revision": "5a907123ff21eb0f771a",
+    "url": "/EthPolls/static/js/runtime~main.cc29c98d.js"
+  },
+  {
+    "revision": "eff49d926c4c3867d9bc",
+    "url": "/EthPolls/static/js/2.9b073ca7.chunk.js"
+  },
+  {
+    "revision": "39c3da9a90f519b3ff08bce35a0c8646",
+    "url": "/EthPolls/index.html"
+  }
+];
